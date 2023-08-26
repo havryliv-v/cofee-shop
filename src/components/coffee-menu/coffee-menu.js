7d2bf88fd4c21d13ac36332762f6b225bc49d94e
Vitalii <havryliv.v@icloud.com>
2023-08-26 11:46:59 +0300

    added react-transition-group to project

--- src/components/coffee-menu/coffee-menu.js
+++ src/components/coffee-menu/coffee-menu.js
@@ -1,27 +1,52 @@
-import CoffeeMenuItem from '../coffee-menu-item/coffee-menu-item';
 
+import coffee from '../../icons/menu.png'
+import Col from 'react-bootstrap/Col';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
+import { CSSTransition, TransitionGroup, SwitchTransition } from 'react-transition-group';
+import { useState, useEffect } from 'react';
+
+import './coffee-menu.scss'
+
 
 const CoffeeMenu = ({ data }) => {
    const elements = data.map(item => {
-      const { id, ...itemProps } = item
+      const { id, name, price } = item
       return (
-         <CoffeeMenuItem
-            key={id}
-            {...itemProps}
-         />
+
+         <CSSTransition key={id} timeout={500} classNames="my-node"  >
+            <Col xs={6} md={4}>
+               <li className="menu__item">
+                  <div className="menu__img"><img src={coffee} alt="coffee" /></div>
+                  <div className="menu__header">AROMISTICO Coffee 1 kg</div>
+                  <div className="menu__item-wrapper">
+                     <span className="menu__type">{name}</span>
+                     <span className="menu__price">{`${price} $`}</span>
+                  </div>
+               </li>
+            </Col>
+         </CSSTransition>
 
       )
    })
+
    return (
-      <div className="menu">
+
+      <ul className="menu">
+
          <Container>
             <Row>
-               {elements}
+
+               <TransitionGroup component={null} >
+
+                  {elements}
+
+               </TransitionGroup>
+
             </Row>
          </Container>
-      </div>
+
+      </ul>
    )
 }
 
